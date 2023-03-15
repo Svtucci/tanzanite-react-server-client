f9b42a13d34f502ed3f7daf975665e391531ce99
Stephen Vertucci <steve.vertucci@gmail.com>
2023-03-14 22:01:11 -0400

    consolidated

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -13,22 +13,6 @@ import Name from '../Name/Name.jsx'
 
 // App is a component, REACT helps to modulize the project
 function App () {
-  // Our functions and variables will go here
-
-  const [firstName, setFirstName] = useState('Chris')
-  // This is the old way of declaring variables 
-  // let firstName = 'Steve';
-  // let counter = 0; we will use something else 
-
-  //   name of var    func        default value
-  
-
-  // const [mystateVariable, setMyStateVariable] = useState('initial value'); 
-
-  // Function to increase click counter 
- 
-
-
 
   return (
     // This looks like HTML but is really JSX
